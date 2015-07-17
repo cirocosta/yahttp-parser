7560b5af7019eb3c4b8d8d9e228fbf6895eb91da
Ciro S. Costa <ciro9758@gmail.com>
2015-07-16 21:59:44 -0300

    removes recently ignored files

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ciro/Development/CPP/yahttp-parser/tests
-# Build directory: /home/ciro/Development/CPP/yahttp-parser/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(parser "parser")
